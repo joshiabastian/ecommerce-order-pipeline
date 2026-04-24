e2579a14dc76ae86b6d6a4b1a533ae45ca925a68
joshiabastian <j.bastian21197@gmail.com>
2026-04-24 21:47:13 +0700

    section dm frautd

--- dbt/models/datamart/dm_top_fraud_users.sql
+++ dbt/models/datamart/dm_top_fraud_users.sql
@@ -15,4 +15,29 @@ with fraud_orders as (
     from {{ ref('fact_orders') }}
     where status = 'frauds'
     group by user_id, user_name, gender, age_group
+),
+ 
+all_orders as (
+    select
+        user_id,
+        count(*) as total_orders
+    from {{ ref('fact_orders') }}
+    group by user_id
 )
+ 
+select
+    f.user_id,
+    f.user_name,
+    f.gender,
+    f.age_group,
+    f.total_fraud_orders,
+    a.total_orders,
+    round(f.total_fraud_orders / a.total_orders * 100, 2) as fraud_rate_pct,
+    f.total_fraud_amount,
+    f.unique_countries,
+    f.first_fraud_date,
+    f.last_fraud_date
+ 
+from fraud_orders f
+left join all_orders a on f.user_id = a.user_id
+order by f.total_fraud_orders desc
